e0b417b949b0a0f62033fdd0150686ac63162539
Monica Garcia <monicairenegarcia@gmail.com>
2023-11-20 20:44:46 -0700

    completed functions lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+return Math.abs(42 - pickup);
+}
+
+function distanceFromHqInFeet(blocks) {
+const feet = distanceFromHqInBlocks(blocks) * 264
+    return  feet;
+}
+
+function distanceTravelledInFeet(start, end) {
+   const totDist = Math.abs(end - start) * 264;
+   return totDist;
+}
+
+function calculatesFarePrice(start, end) {
+const totDist = distanceTravelledInFeet(start, end);
+let fare;
+if (totDist <= 400) {
+    fare = 0;
+    return fare;
+}
+else if (totDist <=2000) {
+    fare = (totDist - 400) * 0.02;
+    return fare;
+}
+else if (totDist <= 2500) {
+    fare = 25;
+    return fare;
+}
+else return "cannot travel that far"
+}
